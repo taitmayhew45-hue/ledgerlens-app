0ad4b59532c03d6a34e26bc6d739df17f6468802
taitmayhew45-hue <taitmayhew45@gmail.com>
2026-01-17 21:11:24 +0200

    Delete app directory

--- app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>LedgerLens AI</h1>
-      <p>This page will allow CSV uploads.</p>
-      <p>App setup successful.</p>
-    </div>
-  );
-}
