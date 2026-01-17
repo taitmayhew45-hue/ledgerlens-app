faec6342a2df9c55873fa9733bfd3b5bde51ef7c
taitmayhew45-hue <taitmayhew45@gmail.com>
2026-01-17 18:39:23 +0200

    Create Home component for the main page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>LedgerLens AI</h1>
+      <p>This page will allow CSV uploads.</p>
+      <p>App setup successful.</p>
+    </div>
+  );
+}
